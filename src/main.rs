5b43ff07e54d66f19521d517dd0f0ff5dfdb8dc2
delaemon <takeshi.music.5@gmail.com>
2015-08-04 02:10:45 +0900

    add prime_list

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,31 @@ fn is_prime(n: i32) -> bool {
     return true;
 }
 
+fn prime_list(n: i32) -> Vec<i32>{
+    let mut dp = vec![false; (n+1) as usize];
+    let mut prime = vec![];
+    let mut i = 2;
+    while i <= n {
+        if dp[i as usize] == false {
+            prime.push(i);
+        }
+        let mut j = i * 2;
+        while j <= n {
+            dp[j as usize] = true;
+            j += i;
+        }
+        i += 1;
+    }
+    return prime;
+}
+
 fn main() {
-    for i in 1..30 {
-        println!("{} is prime {}", i, is_prime(i));
+    for n in 1..10 {
+        println!("{} is prime = {}", n, is_prime(n));
+    }
+
+    let pl = prime_list(10);
+    for p in pl {
+        println!("{}", p);
     }
 }
